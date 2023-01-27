e147953e8e7f494b83de676bbe18b5a299bad780
cypher <m1k4z@yandex.ru>
2023-01-27 10:00:22 +0300

    add unit tests to services

---
+++ Basket.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Basket.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Ordering.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Ordering.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
