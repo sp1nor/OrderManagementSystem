1b6e7f7577837e81391ac412fc65c7d6118d5240
cypher <m1k4z@yandex.ru>
2023-01-27 15:15:55 +0300

    add persistence to program.cs api

--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,11 +1,13 @@
 using Ordering.Application;
 using Ordering.Application.Common.Settings;
+using Ordering.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
 
 builder.Services.AddApplication();
+builder.Services.AddPersistence();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
