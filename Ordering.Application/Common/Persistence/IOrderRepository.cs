fb75a4958395d430107bd70396fd0c5abcc5faf7
cypher <m1k4z@yandex.ru>
2023-01-27 14:08:37 +0300

    add validationBehavior

---
+++ Ordering.Application/Common/Persistence/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Domain.OrderAggregate;
+
+namespace Ordering.Application.Common.Persistence
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetAll();
+        Order GetItemById(int id);
+        void Create(Order item);
+
+        void Update(Order item);
+        void Delete(int id);
+        void Delete(Order item);
+    }
+}
---
+++ Ordering.Application/DepenndencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+using FluentValidation;
+using MediatR;
+using Ordering.Application.Common.Behaviors;
+
+namespace Ordering.Application
+{
+    public static class DepenndencyInjection
+    {
+        public static void AddApplication(this IServiceCollection service)
+        {
+            // add mediatR
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+            // add automapper
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // add validation
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            service.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            // add cache
+            service.AddDistributedMemoryCache();
+            service.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
+        }
+    }
+}
