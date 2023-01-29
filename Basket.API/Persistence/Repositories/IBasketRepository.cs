ab7a1ca6df235c08e010b1ca043a4aa85907bc12
rand <m1k4z@yandex.ru>
2023-01-29 17:18:14 +0300

    add basket repository

--- Basket.API/Persistence/Repositories/IBasketRepository.cs
+++ Basket.API/Persistence/Repositories/IBasketRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Basket.API.Persistence.Repositories
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Persistence.Repositories
 {
-    public class IBasketRepository
+    public interface IBasketRepository
     {
+        Task<Cart> GetBasket(string userName);
+        Task<Cart> UpdateBasket(Cart basket);
+        Task DeleteBasket(string userName);
     }
 }
