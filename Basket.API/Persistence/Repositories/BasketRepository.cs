f1afdf89d603e831c1c951fa50736e7a910b52c4
rand <m1k4z@yandex.ru>
2023-01-29 14:17:47 +0300

    add to basket api redis and update docker-compose

---
+++ Basket.API/Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Basket.API.Persistence.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+    }
+}
--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,9 +1,18 @@
+using Basket.API.Persistence.Repositories;
 using Common.Logging;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(SeriLogger.Configure);
+
+// Redis Configuration
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
